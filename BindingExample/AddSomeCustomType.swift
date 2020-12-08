8ca248d8fa3b4d015c820482861eddcb0eb40e96
Russell Gordon <rgordon@lcs.on.ca>
2020-12-08 08:26:50 -0500

    Set up form for entry of new item details.

--- BindingExample/AddSomeCustomType.swift
+++ BindingExample/AddSomeCustomType.swift
@@ -15,15 +15,41 @@ struct AddSomeCustomType: View {
     // Controls whether this sheet is visible or not
     @Binding var presentingAddItemSheet: Bool
     
+    // The Fields to track details of the new item
+    //
+    // NOTE: Refer to diagram at https://swiftuipropertywrappers.com
+    //
+    // Since the data for these items are created within this view, we use the @State property wrapper
+    @State private var name: String = ""
+    @State private var description: String = ""
+    @State private var flag: Bool = false
+    @State private var rating: Int = 0
+    
     var body: some View {
-
-        VStack {
+        
+        // Since this appears as a sheet (not a view that is navigated to from a list) we use a new navigation view, so that we can show a title for this view
+        NavigationView {
             
-            Text("There are \(items.count) items in the list.")
-            
-            Button("Dismiss This Sheet") {
-                presentingAddItemSheet = false
+            Form {
+                
+                Section {
+                    TextField("Enter item name", text: $name)
+                }
+                
+                Section(header: Text("Description")) {
+                    // A text editor is much like a text field, except it automatically extends to multiple lines to allow for extended input.
+                    TextEditor(text: $description)
+                }
+                
+                Toggle(isOn: $flag) {
+                    Text("Active item?")
+                }
+                
+                Stepper("Out of 10, I rate this a \(rating)", value: $rating, in: 1...10)
+                
             }
+            .navigationTitle("Add New Item")
+    
         }
         
     }
