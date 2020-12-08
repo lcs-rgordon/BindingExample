e1d6280696c0325f5021c474138060d00abe13ec
Russell Gordon <rgordon@lcs.on.ca>
2020-12-08 07:30:22 -0500

    Initial Commit

---
+++ BindingExample/BindingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingExampleApp.swift
+//  BindingExample
+//
+//  Created by Russell Gordon on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct BindingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
